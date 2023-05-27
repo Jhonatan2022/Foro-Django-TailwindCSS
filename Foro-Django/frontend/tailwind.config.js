41324f1ac168c6761e308ffda1bc1e1ca798122d
Jhonattan Florez <101368711+Jhonatan2022@users.noreply.github.com>
2023-05-27 13:56:33 +0100

    Add folders react

---
+++ Foro-Django/frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // Incluimos todos los archivos (html, js) dentro de la carpeta src
+    ".src/**/*.{html,js}'",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
