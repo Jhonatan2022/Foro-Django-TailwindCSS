3520a9341b7c95f83cbec776795a7f717138b93d
Jhonattan Florez <101368711+Jhonatan2022@users.noreply.github.com>
2023-05-27 21:12:28 +0100

    Const Login

---
+++ Foro-Django/frontend/src/store.js
@@ -0,0 +1,36 @@
+// Importamos createStore, combineReducers, applyMiddleware para la creación del store (almacén de datos)
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+
+// Importamos thunk para poder hacer peticiones asíncronas
+import thunk from 'redux-thunk';
+
+// Importamos composeWithDevTools para poder usar la extensión de Redux DevTools
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+// Importamos los reducers que creamos
+import { userLoginReducer } from './reducers/userReducer';
+
+
+
+
+// Creamos el reducer para el login
+const reducer = combineReducers({
+    userLogin: userLoginReducer,
+});
+
+
+
+// Obtenemos el usuario de la sesión
+const userInfoStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null;
+
+
+
+// Creamos el estado inicial
+const initialState = {
+    userLogin: { userInfo: userInfoStorage },
+};
+
+
+
+// Creamos la constante de midelware para poder hacer peticiones asíncronas
+const middleware = [thunk];
